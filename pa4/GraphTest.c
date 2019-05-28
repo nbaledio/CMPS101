df311caee923f0719850e7a030290e3ab1a46417
nbaledio <nbaledio@ucsc.edu>
2019-05-28 02:54:52 -0700

    pa4 complete

--- pa4/GraphTest.c
+++ pa4/GraphTest.c
@@ -1,103 +1,27 @@
-//-----------------------------------------------------------------------------
-//
-// GraphClient.c
-// 
-// This is a fancy test client that calculates the Radius and Diameter of 
-// the graph defined on lines 41-49, along with its Central and Peripheral 
-// vertices.  The definitions of these terms at:
-// 
-//    http://en.wikipedia.org/wiki/Distance_(graph_theory)
-// 
-// Place this file in a directory with copies of your List.c, List.h, Graph.c, 
-// Graph.h and an appropriate Makefile, then compile and run. The output 
-// is contained in the file GraphClientOut.
-// 
-// This program does not exercise all functions in your Graph ADT, but it 
-// does a pretty good job of testing BFS().  If your output differs from 
-// the above, you have a logical problem in either your Graph or List ADT.
-// 
-// Remember you are required to submit a file named GraphTest.c with pa4 that
-// exercises your Graph functions.  Do not submit this file.
-//
-//-----------------------------------------------------------------------------
-
 #include<stdio.h>
 #include<stdlib.h>
-#include"Graph.h"
+#include "Graph.h"
 
 int main(int argc, char* argv[]){
-   int i, s, max, min, d, n=35;
-   List  C = newList(); // central vertices 
-   List  P = newList(); // peripheral vertices 
-   List  E = newList(); // eccentricities 
-   Graph G = NULL;
-
-   // Build graph G 
-   G = newGraph(n);
-   for(int i=1; i<n; i++){
-      if( i%7!=0 ) addEdge(G, i, i+1);
-      if( i<=28  ) addEdge(G, i, i+7);
-   }
-   addEdge(G, 9, 31);
-   addEdge(G, 17, 13);
-   addEdge(G, 14, 33);
-
-   // Print adjacency list representation of G
-   printGraph(stdout, G);
-
-   // Calculate the eccentricity of each vertex 
-   for(s=1; s<=n; s++){
-      BFS(G, s);
-      max = getDist(G, 1);
-      for(i=2; i<=n; i++){
-         d = getDist(G, i);
-         max = ( max<d ? d : max );
-      }
-      append(E, max);
-   }
-
-   // Determine the Radius and Diameter of G, as well as the Central and 
-   // Peripheral vertices.
-   append(C, 1);
-   append(P, 1);
-   min = max = front(E);
-   moveFront(E);
-   moveNext(E);
-   for(i=2; i<=n; i++){
-      d = get(E);
-      if( d==min ){
-         append(C, i);
-      }else if( d<min ){
-         min = d;
-         clear(C);
-         append(C, i);
-      }
-      if( d==max ){
-         append(P, i);
-      }else if( d>max ){
-         max = d;
-         clear(P);
-         append(P, i);
-      }
-      moveNext(E);
-   }
-
-   // Print results 
+   Graph G = newGraph(6);
+   addEdge(G,1,2);
+   addEdge(G,2,4);
+   addEdge(G,2,5);
+   addEdge(G,5,6);
+   addEdge(G,6,3);
+   addEdge(G,2,1);
+   BFS(G,2);
+   List L = newList();
+   getPath(L,G,3);
+   printList(stdout,L);
    printf("\n");
-   printf("Radius = %d\n", min);
-   printf("Central vert%s: ", length(C)==1?"ex":"ices");
-   printList(stdout, C);
-   printf("\n");
-   printf("Diameter = %d\n", max);
-   printf("Peripheral vert%s: ", length(P)==1?"ex":"ices");
-   printList(stdout, P);
-   printf("\n");
-
-   // Free objects 
-   freeList(&C);
-   freeList(&P);
-   freeList(&E);
+   printf("%d\n",getOrder(G));
+   printf("%d\n",getSize(G)); 
+   printf("%d\n",getParent(G,2));
+   printf("%d\n",getParent(G,5));
+   printf("%d\n",getDist(G,3));
+   printf("%d\n",getDist(G,1));
+   printGraph(stdout,G); 
+   freeList(&L);
    freeGraph(&G);
-
-   return(0);
 }
